b787bc9ccf673527794d4a1e4676c98fafd6e306
ruivarandas <48528346+ruivarandas@users.noreply.github.com>
2019-08-01 14:55:56 +0100

    Cleaned backward_elimination.
    Former-commit-id: a0ae85f33df8c02354c0122e9af8259461f5c297

--- Rui/tries_to_upload_pickle.py
+++ Rui/tries_to_upload_pickle.py
@@ -2,6 +2,7 @@ from git import Repo
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
+from time import sleep
 
 def git_push():
     path_to_repo = r"D:\Physionet Challenge\GitHub\pnet2019\.git"
@@ -17,14 +18,24 @@ def git_push():
         print('Git did not work...') 
 
 
-def send_to_drive(file):
+def send_to_drive():
 	try:
         g_login = GoogleAuth()
         g_login.LocalWebserverAuth()
-		with open("grid_search_object.p","r") as file:
+		with open(r"D:\Physionet Challenge\GitHub\pnet2019\Rui\grid_search_object.p","r") as file:
             file_drive = drive.CreateFile({'grid_search_object.p':os.path.basename(file.name) })
             file_drive.SetContentString(file.read())
             file_drive.Upload()
 		print("Drive Worked!")
 	except:
 	    print('Drive did not work...')
+
+
+time_sleep = 3600 * 2
+while "grid_search_object.p" not in os.listdir(r"D:\Physionet Challenge\GitHub\pnet2019\Rui"):
+    time.sleep(time_sleep)
+
+# git_push()
+send_to_drive()
+
+print("It's all over!")
