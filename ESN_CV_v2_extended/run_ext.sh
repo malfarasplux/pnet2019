619f9d638a7f0ec9e4cd3f484ce7de0c756951ff
malfarasplux <malfaras@plux.info>
2019-08-19 18:30:42 +0100

    Extend CV v2 grid search
    Former-commit-id: 34eab9c2881772fc937a444944c6b404759168f7

---
+++ ESN_CV_v2_extended/run_ext.sh
@@ -0,0 +1,113 @@
+## Apply config and run 
+## Loop over scale
+
+sed -i 's:^scale_def.*:scale_def = 0.025   # scaling:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+
+## Loop over mem
+sed -i 's:^mem_def.*:mem_def = 1.1         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.2         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.3         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.4         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.5         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.6         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.7         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.8         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.9         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 2.0         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+
+######################################################################
+sed -i 's:^scale_def.*:scale_def = 0.050   # scaling:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+
+## Loop over mem
+sed -i 's:^mem_def.*:mem_def = 1.1         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.2         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.3         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.4         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.5         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.6         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.7         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.8         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.9         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 2.0         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+######################################################################
+sed -i 's:^scale_def.*:scale_def = 0.075   # scaling:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+
+## Loop over mem
+sed -i 's:^mem_def.*:mem_def = 1.1         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.2         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.3         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.4         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.5         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.6         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.7         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.8         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 1.9         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+sed -i 's:^mem_def.*:mem_def = 2.0         # memory:' ./ESNtrainCV.py
+python3 ./ESNtrainCV.py
+
+
+
